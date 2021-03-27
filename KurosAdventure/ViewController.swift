e0f19121f51995f53343f4d84a2144a2699aba4a
Genki Mine <gomfucius@gmail.com>
2021-03-27 10:28:09 -0700

    Initial Commit

---
+++ KurosAdventure/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  KurosAdventure
+//
+//  Created by Genki Mine on 3/27/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
